13c9f94db4d7a07d6632f004390b7c4cf0a3f3e4
Nabeel Syed <nabeelsyed@Nabeels-MacBook-Pro.local>
2020-11-03 15:25:45 -0500

    Second commit

--- script.py
+++ script.py
@@ -1,13 +1,19 @@
 import pyautogui
+import random
 import time
 pyautogui.FAILSAFE = False
 
 while True:
-    # How many seconds before ghosty.py starts
+    # How many seconds after ghosty.py is ran should the script start
     time.sleep(10)
-    for i in range(0, 100):
+    for i in range(0, 2):
         pyautogui.moveTo(0, i * 5)
-        print('Cursor moved')
+        print("Cursor moved")
         # Sleep for 300 seconds (5 minutes) after each time the cursor is moved
         time.sleep(10)
-    # for i in range(0, 5)
+    for i in range(0, 1):
+        sentences = ["Please excuse me for a moment", "BRB, my package from Amazon just arrived",
+                     "BRB, just making some coffee"]
+        pyautogui.write(random.choice(sentences))
+        pyautogui.press('enter')
+        print("Message typed")
